0b450bdccbb2499351fef0bcf5a5e9fd763bc891
David C <dchung20315@gmail.com>
2023-03-28 18:13:11 -0700

    added random function and partial if else statements

--- script.js
+++ script.js
@@ -3,20 +3,27 @@ var generateBtn = document.querySelector("#generate");
 
 /*Password Length Criteria*/
 
+var passwordLengthBool = false;
+var passwordLength;
+var lowercasePassword;
+var uppercasePassword;
+var numbersPassword;
+var specialPassword;
+
 function selectPasswordLength() {
   var passwordLength = prompt("Please enter the length of your desired password", "8-128");
   if (passwordLength !== null && passwordLength >= 8 && passwordLength <= 128) {
-    window.prompt("Your password length is " + passwordLength + ".");
+    window.alert("Your password length is " + passwordLength + ".");
+    passwordLengthBool = true;
     return passwordLength;
   }
   else {
-    window.prompt("Invalid password length.");
+    window.alert("Invalid password length.");
   }
 }
 
-selectPasswordLength();
-
 /*lowercase allowed prompt*/
+
 function selectLowercase() {
   var lowercasePassword;
   if (confirm("Lowercase?")) {
@@ -29,9 +36,8 @@ function selectLowercase() {
   }
 }
 
-selectLowercase();
-
 /*uppercase allowed prompt*/
+
 function selectUppercase() {
   var uppercasePassword;
   if (confirm("Uppercase?")) {
@@ -44,9 +50,8 @@ function selectUppercase() {
   }
 }
 
-selectUppercase();
-
 /*numbers allowed prompt*/
+
 function selectNumbers() {
   var numbersPassword;
   if (confirm("Numbers?")) {
@@ -59,9 +64,8 @@ function selectNumbers() {
   }
 }
 
-selectNumbers();
-
 /*special characters allowed prompt */
+
 function selectSpecial() {
   var specialPassword;
   if (confirm("Special Characters?")) {
@@ -74,9 +78,50 @@ function selectSpecial() {
   }
 }
 
+/*Running prompts and alerts functions*/
+
+while (passwordLengthBool !== true) {
+  selectPasswordLength();
+}
+selectLowercase();
+selectUppercase();
+selectNumbers();
 selectSpecial();
+console.log(specialPassword);
 
+/*Define arrays to pull from for each character */
+var lowercaseCharSet = ["a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l", "m", "n", "o", "p", "q", "r", "s", "t", "u", "v", "w", "x", "y", "z"];
+var uppercaseCharSet = ["A", "B", "C", "D", "E", "F", "G", "H", "I", "J", "K", "L", "M", "N", "O", "P", "Q", "R", "S", "T", "U", "V", "W", "X", "Y", "Z"];
+var numberCharSet = [0, 1, 2, 3, 4, 5, 6, 7, 8, 9]; /* 10 */
+var specialCharSet = ["!", "#", "$", "%", "&", "'", "(", ")", "*", "+", ",", "-", ".", "/", ":", ";", "<", ">", "=", "?", "@", "[", "]", "^", "\u005C", "_", "\u0022", "`", "{", "}", "|", "~"];
 
+/*random number generator function*/
+function getRndNum(min, max) {
+  return Math.floor(Math.random() * (max-min));
+}
+
+/*
+for (i=0; i < passwordLength; i++){
+  if (getRndNum(0,4) === 0){
+    var lowercaseLetter = lowercaseCharSet[getRndNum(0, lowercaseCharSet.length)];
+    return lowercaseLetter;
+  }
+
+  else if (getRndNum(0,4) === 1){
+    var uppercaseLetter = uppercaseCharSet[getRndNum(0, uppercaseCharSet.length)];
+    return uppercaseLetter;
+  }
+
+  else if (getRndNum(0,4) === 2){
+    var number = numberCharSet[getRndNum(0, numberCharSet.length)];
+    return number;
+  }
+
+  else {
+    var specialCharacter = specialCharSet[getRndNum(0, specialCharSet.length)];
+    return specialCharacter;
+  }
+} */
 
 // Write password to the #password input
 function writePassword() {
@@ -84,10 +129,6 @@ function writePassword() {
   var passwordText = document.querySelector("#password");
 
   passwordText.value = password;
-
-}
-
-function generatePassword() {
 
 }
 
