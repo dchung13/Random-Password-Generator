eab85357000532398e6ce975a8b18d9e9e7183e0
David C <dchung20315@gmail.com>
2023-03-30 15:15:49 -0700

    added charset and added lower,upper,number,special charset if true

--- script.js
+++ script.js
@@ -2,125 +2,129 @@
 var generateBtn = document.querySelector("#generate");
 
 /*Password Length Criteria*/
+function generatePassword () {
 
-var passwordLengthBool = false;
-var passwordLength;
-var lowercasePassword = selectLowercase();
-var uppercasePassword;
-var numbersPassword;
-var specialPassword;
-
-function selectPasswordLength() {
-  var passwordLength = prompt("Please enter the length of your desired password", "8-128");
-  if (passwordLength !== null && passwordLength >= 8 && passwordLength <= 128) {
-    window.alert("Your password length is " + passwordLength + ".");
-    passwordLengthBool = true;
-    return passwordLength;
-  }
-  else {
-    window.alert("Invalid password length.");
-  }
-}
-
-/*lowercase allowed prompt*/
-
-function selectLowercase() {
-  if (confirm("Lowercase?")) {
-    window.alert("Lowercase allowed.")
-    return lowercasePassword = true;
-  }
-  else {
-    window.alert("Lowercase not allowed.")
-    return lowercasePassword = false;
-  }
-}
-
-/*uppercase allowed prompt*/
-
-function selectUppercase() {
+  var passwordLengthBool = false;
+  var passwordLength;
+  var lowercasePassword;
   var uppercasePassword;
-  if (confirm("Uppercase?")) {
-    window.alert("Uppercase allowed.")
-    return uppercasePassword = true;
-  }
-  else {
-    window.alert("Uppercase not allowed.")
-    return uppercasePassword = false;
-  }
-}
-
-/*numbers allowed prompt*/
-
-function selectNumbers() {
   var numbersPassword;
-  if (confirm("Numbers?")) {
-    window.alert("Numbers allowed.")
-    return numbersPassword = true;
-  }
-  else {
-    window.alert("Numbers not allowed.")
-    return numbersPassword = false;
-  }
-}
-
-/*special characters allowed prompt */
-
-function selectSpecial() {
   var specialPassword;
-  if (confirm("Special Characters?")) {
-    window.alert("Special characters allowed.")
-    return specialPassword = true;
+
+  function selectPasswordLength() {
+    passwordLength = prompt("Please enter the length of your desired password", "8-128");
+    if (passwordLength < 8 || passwordLength > 128 || isNaN(passwordLength)) {
+      window.alert("Invalid password length.");
+    }
+    else {
+      window.alert("Your password length is " + passwordLength + ".");
+      passwordLengthBool = true;
+      return passwordLength;
+    }
   }
-  else {
-    window.alert("Special characters not allowed.")
-    return specialPassword = false;
+
+  /*lowercase allowed prompt*/
+
+  function selectLowercase() {
+    if (confirm("Lowercase?")) {
+      window.alert("Lowercase allowed.")
+      return lowercasePassword = true;
+    }
+    else {
+      window.alert("Lowercase not allowed.")
+      return lowercasePassword = false;
+    }
   }
+
+
+  /*uppercase allowed prompt*/
+
+  function selectUppercase() {
+    if (confirm("Uppercase?")) {
+      window.alert("Uppercase allowed.")
+      return uppercasePassword = true;
+    }
+    else {
+      window.alert("Uppercase not allowed.")
+      return uppercasePassword = false;
+    }
+  }
+
+  /*numbers allowed prompt*/
+
+  function selectNumbers() {
+    if (confirm("Numbers?")) {
+      window.alert("Numbers allowed.")
+      return numbersPassword = true;
+    }
+    else {
+      window.alert("Numbers not allowed.")
+      return numbersPassword = false;
+    }
+  }
+
+  /*special characters allowed prompt */
+
+  function selectSpecial() {
+    if (confirm("Special Characters?")) {
+      window.alert("Special characters allowed.")
+      return specialPassword = true;
+    }
+    else {
+      window.alert("Special characters not allowed.")
+      return specialPassword = false;
+    }
+  }
+
+ 
+  /*Running prompts and alerts functions*/
+
+  while (passwordLengthBool !== true) {
+    selectPasswordLength();
+  }
+  selectLowercase();
+  selectUppercase();
+  selectNumbers();
+  selectSpecial();
+
+  console.log(passwordLength);
+  console.log(lowercasePassword);
+  console.log(uppercasePassword);
+  console.log(numbersPassword);
+  console.log(specialPassword);
+
+  /*Define character sets*/
+  var lowercaseCharSet = "abcdefghijklmnopqrstuvwxyz";
+  var uppercaseCharSet = "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
+  var numberCharSet = "0123456789"
+  var specialCharSet = "!#$%&'()*+,-./:;<>=?@[]^\u005C_\u0022`{}|~";
+
+
+  var passwordCharSet = "";
+
+  if (lowercasePassword) {
+    passwordCharSet += lowercaseCharSet;
+  }
+  if (uppercasePassword) {
+    passwordCharSet += uppercaseCharSet;
+  }
+  if (numbersPassword) {
+    passwordCharSet += numberCharSet;
+  }
+  if (specialPassword) {
+    passwordCharSet += specialCharSet;
+  }
+
+
+
+  var password = "";
+  for (i=0; i < passwordLength; i++) {
+    var randomIndex = Math.floor(Math.random() * passwordCharSet.length);
+    password += passwordCharSet[randomIndex];
+  }
+
+  return password;
 }
-
-/*Running prompts and alerts functions*/
-
-while (passwordLengthBool !== true) {
-  selectPasswordLength();
-}
-selectLowercase();
-selectUppercase();
-selectNumbers();
-selectSpecial();
-console.log(specialPassword);
-
-/*Define arrays to pull from for each character */
-var lowercaseCharSet = ["a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l", "m", "n", "o", "p", "q", "r", "s", "t", "u", "v", "w", "x", "y", "z"];
-var uppercaseCharSet = ["A", "B", "C", "D", "E", "F", "G", "H", "I", "J", "K", "L", "M", "N", "O", "P", "Q", "R", "S", "T", "U", "V", "W", "X", "Y", "Z"];
-var numberCharSet = [0, 1, 2, 3, 4, 5, 6, 7, 8, 9]; /* 10 */
-var specialCharSet = ["!", "#", "$", "%", "&", "'", "(", ")", "*", "+", ",", "-", ".", "/", ":", ";", "<", ">", "=", "?", "@", "[", "]", "^", "\u005C", "_", "\u0022", "`", "{", "}", "|", "~"];
-
-/*random number generator function*/
-function getRndNum(min, max) {
-  return Math.floor(Math.random() * (max-min));
-}
-
-
-for (i=0; i < passwordLength; i++){
-  if (getRndNum(0,4) === 0){
-    var lowercaseLetter = lowercaseCharSet[getRndNum(0, lowercaseCharSet.length)];
-    return lowercaseLetter;
-  }
-
-  else if (getRndNum(0,4) === 1){
-    var uppercaseLetter = uppercaseCharSet[getRndNum(0, uppercaseCharSet.length)];
-    return uppercaseLetter;
-  }
-
-  else if (getRndNum(0,4) === 2){
-    var number = numberCharSet[getRndNum(0, numberCharSet.length)];
-    return number;
-  }
-
-  else {
-    var specialCharacter = specialCharSet[getRndNum(0, specialCharSet.length)];
-    return specialCharacter;
-  }
-} 
 
 // Write password to the #password input
 function writePassword() {
@@ -133,4 +137,3 @@ function writePassword() {
 
 // Add event listener to generate button
 generateBtn.addEventListener("click", writePassword);
-
